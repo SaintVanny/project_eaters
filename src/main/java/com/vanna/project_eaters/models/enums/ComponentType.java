26ce3bbb387de4312a602dedd7318eb645e1482c
SaintVanny <ironhelm_13@mail.ru>
2025-04-30 17:38:24 +0300

    add ComponentType

---
+++ src/main/java/com/vanna/project_eaters/models/enums/ComponentType.java
@@ -0,0 +1,5 @@
+package com.vanna.project_eaters.models.enums;
+
+public enum ComponentType {
+    PROTEIN, FAT, CARBOHYDRATE, OTHER
+}
--- src/main/java/com/vanna/project_eaters/models/enums/Gender.java
+++ src/main/java/com/vanna/project_eaters/models/enums/Gender.java
@@ -3,3 +3,4 @@ package com.vanna.project_eaters.models.enums;
 public enum Gender {
     MALE,FEMALE
 }
+
