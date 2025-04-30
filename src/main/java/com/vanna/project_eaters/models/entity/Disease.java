cdf1ebb222369ddccd75b97fc59da4ff186267d1
SaintVanny <ironhelm_13@mail.ru>
2025-04-30 16:31:04 +0300

    add Disease

---
+++ src/main/java/com/vanna/project_eaters/models/entity/Disease.java
@@ -0,0 +1,21 @@
+package com.vanna.project_eaters.models.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+public class Disease {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToMany
+    private List<Rule> rules;
+}
